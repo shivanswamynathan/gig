4d66c12743a61b62450285941cf4258a797bd316
shivanswamynathan <46991694+shivanswamynathan@users.noreply.github.com>
2025-06-24 14:45:29 +0530

    Update urls.py

--- reconciliation/document_processing/urls.py
+++ reconciliation/document_processing/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import views,po_grn_views
+from .views import views,po_grn_views,itemwise_grn_views
 
 app_name = 'document_processing'
 
@@ -8,5 +8,9 @@ urlpatterns = [
 
     # PO-GRN data processing
     path('api/process-po-grn/', po_grn_views.ProcessPoGrnAPI.as_view(), name='process_po_grn'),
+
+    # Item-wise GRN data processing
+    path('api/process-itemwise-grn/', itemwise_grn_views.ProcessItemWiseGrnAPI.as_view(), name='process_itemwise_grn'),
+
     
 ]
